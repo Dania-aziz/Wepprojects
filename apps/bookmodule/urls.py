f00dc5587f7806e4377e561d58001765420abd51
Dania-aziz <daniaaziz5040@gmail.com>
2025-09-21 00:14:55 +0300

    Update urls.py

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),  # هذا السطر مهم
+    path('', views.index), 
     path('index2/<int:val1>/', views.index2),
-    path('<int:bookId>/', views.viewbook),  # أضف هذا السطر
+    path('<int:bookId>/', views.viewbook),
 
 ]
